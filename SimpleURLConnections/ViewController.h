dc21e34a314596fc9ad998a510cb0a8dac14428a
MrChen <chenshilongfly@sina.com>
2018-06-11 15:53:31 +0800

    first commit

---
+++ SimpleURLConnections/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleURLConnections
+//
+//  Created by MrChen on 2018/6/11.
+//  Copyright © 2018 wangsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SimpleURLConnections/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SimpleURLConnections
+//
+//  Created by MrChen on 2018/6/11.
+//  Copyright © 2018 wangsu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
